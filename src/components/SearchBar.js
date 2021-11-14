3f1984a13d551708bcfd9ff3b4f8be5f3a446d36
jreyes8631 <jreyes8631@gmail.com>
2021-11-14 11:10:31 -0500

    attaching searchBar component to the App component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class SearchBar extends React.Component {
    render() {
-       return <div>SearchBar Component</div>
+       return <div>Search Bar Component</div>
    }
 }
 
