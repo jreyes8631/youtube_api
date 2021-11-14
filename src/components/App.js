ae55f467f765250eeb49ad3c90ce3181adfbcc24
jreyes8631 <jreyes8631@gmail.com>
2021-11-14 11:37:38 -0500

    add margin to search bar from app component

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,9 @@ import SearchBar from './SearchBar';
 class App extends React.Component {
   render() {
     return(
-      <div><SearchBar/></div>
+      <div className="ui container">
+       <SearchBar/>
+      </div>
     )
   }
 
