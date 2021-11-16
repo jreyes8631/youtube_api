f5466aef372333929d9fd2d02c9966cd221a1e8a
jreyes8631 <jreyes8631@gmail.com>
2021-11-16 16:07:56 -0500

    add axios

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = "AIzaSyBk88UFfAd4xy8f_e86ZtkbpPbG_4k8jj0";
+
+export default axios.createService({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+      part: 'snippet',
+      maxResults: 5,
+      key: KEY
+  }
+});
