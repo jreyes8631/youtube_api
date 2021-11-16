9406158831c9957e207a9d350bafa3c2c2d05f91
jreyes8631 <jreyes8631@gmail.com>
2021-11-16 14:16:54 -0500

    add value to text field in search bar and handle the submit functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = {text: " "}
 
+  onInputChange = (e) => {
+    this.setState({text: e.target.value})
+  };
+
+  onFormSubmit= (e) => {
+    e.preventDefault()
+  }
 
    render() {
        return (
        <div className="search-bar ui segment">
-           <form className="ui form">
+           <form onSubmit={this.onFormSubmit} className="ui form">
                <div className="field">
                    <label>
                        Video Search
                    </label>
-                   <input type="text" />
+                   <input type="text" 
+                   value={this.state.text}
+                   onChange={this.onInputChange}
+                   />
                </div>
            </form>
        </div>
