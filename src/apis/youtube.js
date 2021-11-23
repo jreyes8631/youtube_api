8616e4704ce605db6b5389e517ccc7e4b67638d1
jreyes8631 <jreyes8631@gmail.com>
2021-11-23 15:03:31 -0500

    fix create action

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const KEY = "AIzaSyBk88UFfAd4xy8f_e86ZtkbpPbG_4k8jj0";
 
-export default axios.createService({
+export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
       part: 'snippet',
