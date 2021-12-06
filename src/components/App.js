143a61f5be43bf364b91f8add6e4ca9f18409027
jreyes8631 <jreyes8631@gmail.com>
2021-12-06 18:04:07 -0500

    SelectedVideo is going to change after a new search

--- src/components/App.js
+++ src/components/App.js
@@ -15,8 +15,13 @@ class App extends React.Component {
      }
   });
 
-   this.setState({ videos: response.data.items})
+   this.setState({ 
+     videos: response.data.items,
+     selectedVideo: response.data.items[0]
+    })
  };
+
+ 
 
  onVideoSelect = async (video) => {
    this.setState({ selectedVideo: video})
